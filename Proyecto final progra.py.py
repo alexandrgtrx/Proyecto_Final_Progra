d42c4ed3a9ba9bacd4b4c9842fb5f00f1d4f9e71
Geovany Yac <yovani.yac56@gmail.com>
2023-05-22 09:15:11 -0600

    Creacion de pantalla principal para el juego Matriz aritmetica y cambio del nombre y el icono del proyecto

--- Proyecto final progra.py.py
+++ Proyecto final progra.py.py
@@ -1,1 +1,7 @@
-print('HOla nalgonas')
+from tkinter import *
+raiz=Tk()
+raiz.title("Matriz Aritmetica")
+raiz.geometry("450x450")
+raiz.resizable(0,0)
+raiz.iconbitmap("Logo-Meso-Color.ico")
+raiz.mainloop()
